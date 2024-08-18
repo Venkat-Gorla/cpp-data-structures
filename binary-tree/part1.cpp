5bb092edb1c0e3d8832aad8d1ef9f1a5df5012c0
Venkat Gorla <venkatagorla@gmail.com>
2024-08-18 11:07:06 +0530

    Update part1.cpp, binary tree find inorder successor

--- binary-tree/part1.cpp
+++ binary-tree/part1.cpp
@@ -302,3 +302,101 @@ Actual output:
 2 1 3 4
 
 */
+
+//----------------------------------------
+// given a binary tree and a key (that may or may not be present in it) find the key's Inorder successor
+
+/*
+    1
+   /  \
+   2   3
+
+Inorder: 2 1 3
+
+Key  Output
+2    1
+1    3
+3    null
+
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* Create(int value)
+{
+    return new Node{value, nullptr, nullptr};
+}
+
+// we will solve this problem using recursion, a different solution using iteration
+// will be discussed separately.
+
+// The idea is to keep track of the previously visited node while doing recursive Inorder;
+// In C++ it can be done with a double pointer i.e. pointer to pointer
+Node* inorderSuccessor(
+    Node* current,
+    const int K,
+    Node** prev)
+{
+    if (!current)
+        return nullptr;
+
+    auto result = inorderSuccessor(current->left, K, prev);
+    if (result)
+        return result;
+
+    if (*prev && (*prev)->data == K)
+        return current;
+
+    *prev = current;
+
+    return inorderSuccessor(current->right, K, prev);
+}
+
+Node* inorderSuccessor(Node* root, const int K)
+{
+    Node* prev{nullptr};
+
+    return inorderSuccessor(root, K, &prev);
+}
+
+void testcase(Node* root, const int K)
+{
+    auto result = inorderSuccessor(root, K);
+    if (result)
+        cout << "Inorder successor for key " << K << " is " << result->data << endl;
+    else
+        cout << "Inorder successor for key " << K << " Not found" << endl;
+}
+
+int main(int argc, char *argv[])
+{ 
+    {
+        Node* root = Create(1);
+        root->left = Create(2);
+        root->right = Create(3);
+        
+        testcase(root, 2);
+        testcase(root, 1);
+        testcase(root, 3);
+    }
+
+    return 0;
+}
+
+/*
+Expected output:
+
+Actual output:
+Inorder successor for key 2 is 1
+Inorder successor for key 1 is 3
+Inorder successor for key 3 Not found
+
+*/
