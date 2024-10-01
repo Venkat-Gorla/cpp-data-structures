4fe2237f4c73dad316d023bf0b6fe433a312d2ca
Venkat Gorla <venkatagorla@gmail.com>
2024-10-01 17:16:59 +0530

    Update 02-general.cpp, some additional binary tree problems and solutions

--- binary-tree/02-general.cpp
+++ binary-tree/02-general.cpp
@@ -186,3 +186,116 @@ Inorder successor for key 1 is 3
 Inorder successor for key 3 Not found
 
 */
+
+//----------------------------------------
+// The problem statement for some of the questions discussed here were taken from here:
+// http://cslibrary.stanford.edu/110/BinaryTrees.pdf
+// hasPathSum
+
+/*
+Given a binary tree and a sum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum. Return false if no such path can be found.
+
+Sample tree with negative values for testing:
+            3 
+          /   \
+          1     5 
+           \    /
+            2  -4 
+
+Root to leaf paths:
+  3 1 2 , sum = 6
+  3 5 -4 , sum = 4
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* Create(int value)
+{
+    return new Node{value, nullptr, nullptr};
+}
+
+// fwd declarations
+void TestPathSum(Node* root, int sum);
+bool IsLeaf(const Node* current);
+// end fwd declarations
+
+// approach:
+// as you go down the tree, you subtract the current node's value from the sum;
+// if you reach a leaf, the sum should be 0 for the success case
+//
+// Note: if the tree is empty (null), this function will return false for all cases,
+// including if the input sum is 0; to return true for such a case, it can be done
+// outside this function.
+bool hasPathSum(Node* root, int sum)
+{
+    if (root == nullptr)
+        return false;
+
+    sum -= root->data;
+
+    if (IsLeaf(root))
+        return sum == 0;
+
+    return hasPathSum(root->left, sum) || hasPathSum(root->right, sum);
+}
+
+int main(int argc, char *argv[])
+{ 
+    {
+        /*
+            3 
+          /   \
+          1     5 
+           \    /
+            2  -4 
+
+        */
+
+        Node* root = Create(3);
+        root->left = Create(1);
+        root->right = Create(5);
+        root->left->right = Create(2);
+        root->right->left = Create(-4);
+
+        // success cases
+        TestPathSum(root, 6);
+        TestPathSum(root, 4);
+
+        // failure cases
+        TestPathSum(root, 10);
+        TestPathSum(root, -20);
+    }
+
+    return 0;
+}
+
+void TestPathSum(Node* root, int sum)
+{
+    if (hasPathSum(root, sum))
+        cout << "Root to leaf path with sum " << sum << " is found" << endl;
+    else
+        cout << "Root to leaf path with sum " << sum << " is *Not* found" << endl;
+}
+
+// caller should ensure "current" is not null
+bool IsLeaf(const Node* current)
+{
+    return !(current->left || current->right);
+}
+
+/*
+Output:
+Root to leaf path with sum 6 is found
+Root to leaf path with sum 4 is found
+Root to leaf path with sum 10 is *Not* found
+Root to leaf path with sum -20 is *Not* found
+
+*/
