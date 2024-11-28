31316b6a961cf98cc8307bc45e318234f36455f5
Venkat Gorla <venkatagorla@gmail.com>
2024-11-28 12:09:51 +0530

    Reverse linked list using iteration and O(1) space

--- linked-list/03-problems-solutions.cpp
+++ linked-list/03-problems-solutions.cpp
@@ -308,3 +308,120 @@ Alternate list: 1 3 5 5 3 1
 
 */
 
+//----------------------------------------
+// - Given a linked list, write a function to reverse it using O(1) space
+
+#include <iostream>
+#include <vector>
+#include <numeric> // iota
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next; // for auto memory management, consider using unique_ptr
+};
+
+Node* create(int value)
+{
+    return new Node{value, nullptr};
+}
+
+Node* createList(const vector<int> & input)
+{
+    Node* head{nullptr};
+    Node* tail{nullptr};
+
+    for (auto value : input)
+    {
+        if (head == nullptr)
+        {
+            head = create(value);
+            tail = head;
+        }
+        else
+        {
+            tail->next = create(value);
+            tail = tail->next;
+        }
+    }
+
+    return head;
+}
+
+void printList(Node* head)
+{
+    cout << "Printing list: ";
+    for (auto current = head; current; current = current->next)
+    {
+        cout << current->data << " ";
+    }
+    cout << endl;
+}
+// end list creation and helper functions
+
+// solution function
+// we will look at a different approach using a previous pointer
+// without using the stack "push" function presented earlier in list intro
+Node* listReverse(Node* head)
+{
+    // special casing not needed for empty list
+
+    Node* current{head};
+    Node* previous{nullptr};
+
+    while (current)
+    {
+        Node* next{current->next};
+
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+
+    return previous;
+}
+
+// fwd declarations
+void testReverseList(const vector<int> & input);
+// end fwd declarations
+
+int main(int argc, char *argv[])
+{
+    {
+        const vector<int> empty{};
+        testReverseList(empty);
+    }
+
+    {
+        cout << endl;
+        vector<int> input(5);
+        iota(input.begin(), input.end(), 1);
+        testReverseList(input);
+    }
+
+    return 0;
+}
+
+void testReverseList(const vector<int> & input)
+{
+    Node* head{createList(input)};
+    printList(head);
+
+    head = listReverse(head);
+    cout << "Printing reversed list" << endl;
+    printList(head);
+}
+
+/*
+Output:
+Printing list:
+Printing reversed list
+Printing list:
+
+Printing list: 1 2 3 4 5
+Printing reversed list
+Printing list: 5 4 3 2 1
+
+*/
+
