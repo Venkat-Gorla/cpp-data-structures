e0cbb3caa2c6c912f15f10ee96dc59e995c24c4e
Venkat Gorla <venkatagorla@gmail.com>
2024-11-29 17:31:16 +0530

    Given a linked list, write a function to reverse every k nodes

--- linked-list/03-problems-solutions.cpp
+++ linked-list/03-problems-solutions.cpp
@@ -361,7 +361,7 @@ void printList(Node* head)
 // end list creation and helper functions
 
 // solution function
-// we will look at a different approach using a previous pointer
+// we will look at a different approach using a previous pointer,
 // without using the stack "push" function presented earlier in list intro
 Node* listReverse(Node* head)
 {
@@ -422,6 +422,130 @@ Printing list:
 Printing list: 1 2 3 4 5
 Printing reversed list
 Printing list: 5 4 3 2 1
+
+*/
+
+//----------------------------------------
+// - Given a linked list, write a function to reverse every k nodes (where k is an input to the function).
+
+/*
+Example:
+Inputs:  1->2->3->4->5->6->7->8->NULL and k = 3 
+Output:  3->2->1->6->5->4->8->7->NULL. 
+
+Inputs:   1->2->3->4->5->6->7->8->NULL and k = 5
+Output:  5->4->3->2->1->8->7->6->NULL. 
+*/
+
+#include <iostream>
+#include <vector>
+#include <numeric> // iota
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next; // for auto memory management, consider using unique_ptr
+};
+
+Node* create(int value)
+{
+    return new Node{value, nullptr};
+}
+
+Node* createList(const vector<int> & input)
+{
+    Node* head{nullptr};
+    Node* tail{nullptr};
+
+    for (auto value : input)
+    {
+        if (head == nullptr)
+        {
+            head = create(value);
+            tail = head;
+        }
+        else
+        {
+            tail->next = create(value);
+            tail = tail->next;
+        }
+    }
+
+    return head;
+}
+
+void printList(Node* head)
+{
+    cout << "Printing list: ";
+    for (auto current = head; current; current = current->next)
+    {
+        cout << current->data << " ";
+    }
+    cout << endl;
+}
+// end list creation and helper functions
+
+// solution function using recursion
+Node* KReverse(Node* head, const int K)
+{
+    if (head == nullptr)
+        return head;
+
+    Node* previous{nullptr};
+    Node* current{head};
+
+    for (int index = 0; current && index < K; index++)
+    {
+        Node* next{current->next};
+
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+
+    head->next = KReverse(current, K);
+
+    return previous;
+}
+
+// fwd declarations
+void testKReverse(const vector<int> & input, const int K);
+// end fwd declarations
+
+int main(int argc, char *argv[])
+{
+    {
+        vector<int> input(9);
+        iota(input.begin(), input.end(), 1);
+        testKReverse(input, 3);
+
+        cout << endl;
+        testKReverse(input, 5);
+    }
+
+    return 0;
+}
+
+void testKReverse(const vector<int> & input, const int K)
+{
+    Node* head{createList(input)};
+    printList(head);
+
+    head = KReverse(head, K);
+    cout << "Printing K(=" << K << ") reversed list" << endl;
+    printList(head);
+}
+
+/*
+Output:
+Printing list: 1 2 3 4 5 6 7 8 9
+Printing K(=3) reversed list
+Printing list: 3 2 1 6 5 4 9 8 7
+
+Printing list: 1 2 3 4 5 6 7 8 9
+Printing K(=5) reversed list
+Printing list: 5 4 3 2 1 9 8 7 6
 
 */
 
